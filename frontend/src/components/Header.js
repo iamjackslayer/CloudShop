6eeadf9295ce9bea8577ccb9e18e45e28524b446
Jack Wong <jackwong3101@yahoo.com>
2020-12-05 15:46:58 +0800

    remove eslintcache

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
@@ -7,22 +8,24 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand>CloudShop</Navbar.Brand>
-          </LinkContainer>
+          {/* <LinkContainer to='/'> */}
+          <Navbar.Brand as={Link} to='/'>
+            CloudShop
+          </Navbar.Brand>
+          {/* </LinkContainer> */}
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <LinkContainer to='/cart'>
-                <Nav.Link>
-                  <i className='fas fa-shopping-cart'></i> Cart
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fas fa-user'></i> Sign in
-                </Nav.Link>
-              </LinkContainer>
+              {/* <LinkContainer to='/cart'> */}
+              <Nav.Link as={Link} to='/cart'>
+                <i className='fas fa-shopping-cart'></i> Cart
+              </Nav.Link>
+              {/* </LinkContainer> */}
+              {/* <LinkContainer to='/login'> */}
+              <Nav.Link as={Link} to='/login'>
+                <i className='fas fa-user'></i> Sign in
+              </Nav.Link>
+              {/* </LinkContainer> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
