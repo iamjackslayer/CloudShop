4b0ace05d68a37fad0522b0803bf39a849ad5a27
Jack Wong <jackwong3101@yahoo.com>
2020-12-08 13:47:41 +0800

    Create productControllers abstract away the implementation of controller from router

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,18 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+import {
+  getProducts,
+  getProductById
+} from '../controllers/productController.js'
 const router = express.Router()
 
 // @desc  Fetch all products
 // @route  GET /api/products
 // @access  Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    let products = await Product.find({})
-    res.json(products)
-  })
-)
+router.get('/', getProducts)
 
 // @desc  Fetch single product
 // @route  GET /api/products/:id
 // @access  Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    let product = await Product.findById(req.params.id)
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found') // our errorHandler will be called
-    }
-  })
-)
+router.get('/:id', getProductById)
 
 export default router
