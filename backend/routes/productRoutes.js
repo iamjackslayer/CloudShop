cad0e56eb33a882ac8cd90ad21291f491e12b57a
Jack Wong <jackwong3101@yahoo.com>
2020-12-08 13:57:25 +0800

    User router.route().get() instead of router.get()

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,3 +1,4 @@
+// Router points to controller
 import express from 'express'
 import {
   getProducts,
@@ -8,11 +9,11 @@ const router = express.Router()
 // @desc  Fetch all products
 // @route  GET /api/products
 // @access  Public
-router.get('/', getProducts)
+router.route('/').get(getProducts)
 
 // @desc  Fetch single product
 // @route  GET /api/products/:id
 // @access  Public
-router.get('/:id', getProductById)
+router.route('/:id').get(getProductById)
 
 export default router
