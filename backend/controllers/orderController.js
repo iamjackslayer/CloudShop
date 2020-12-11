6e765cb9ac1be534771c078238bd9b17f2fe270b
Jack Wong <jackwong3101@yahoo.com>
2020-12-12 03:34:46 +0800

    Create patch route to update order to paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,3 +54,30 @@ export const getOrderById = asyncHandler(async (req, res) => {
   }
   res.status(200).json(order)
 })
+
+// @desc Update order to paid
+// @route PATCH /api/orders/:id/pay
+// @access Private
+export const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (_.isEmpty(order)) {
+    res.status(404)
+    throw new Error('Order not found') // our errorHandler will be called
+  }
+  if (order.user._id.toString() !== req.user._id.toString()) {
+    res.status(401)
+    throw new Error('Unauthorized')
+  }
+  order.isPaid = true
+  order.paidAt = Date.now()
+  order.paymentResult = {
+    // comes from PayPal in req.body
+    id: req.body.id,
+    status: req.body.status,
+    update_time: req.body.update_time,
+    email_address: req.body.payer.email_address
+  }
+  const updatedOrder = await order.save()
+  res.status(200).json(updatedOrder)
+})
