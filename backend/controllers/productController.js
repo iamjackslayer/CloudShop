d5daf9e43ce89ccbcbf922f9738f7939392a1d0a
Jack Wong <jackwong3101@yahoo.com>
2020-12-15 14:59:54 +0800

    create POST /api/products/ and PUT /api/products/:id routes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -35,3 +35,51 @@ export const deleteProductById = asyncHandler(async (req, res) => {
     throw new Error('Product not found') // our errorHandler will be called
   }
 })
+
+// @desc Create a product
+// @route POST /api/products/:id
+// @access Private/Admin
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = await Product.create({
+    user: req.user._id,
+    name: 'Sample name',
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    description: 'Sample description',
+    price: 0,
+    countInStock: 0
+  })
+
+  res.status(201).json(product)
+})
+
+// @desc Update a product
+// @route PUT /api/products/
+// @access Private/Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+  const toUpdate = await Product.findById(req.params.id)
+  const {
+    name,
+    image,
+    brand,
+    category,
+    description,
+    price,
+    countInStock
+  } = req.body
+  if (!toUpdate) {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+  if (name) toUpdate.name = name
+  if (image) toUpdate.image = image
+  if (brand) toUpdate.brand = brand
+  if (category) toUpdate.category = category
+  if (description) toUpdate.description = description
+  if (price) toUpdate.price = price
+  if (countInStock) toUpdate.countInStock = countInStock
+
+  await toUpdate.save()
+  res.json(toUpdate)
+})
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,16 +3,19 @@ import express from 'express'
 import {
   getProducts,
   getProductById,
-  deleteProductById
+  deleteProductById,
+  createProduct,
+  updateProduct
 } from '../controllers/productController.js'
 import { admin, protect } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 
 router
   .route('/:id')
   .get(getProductById)
   .delete(protect, admin, deleteProductById)
+  .put(protect, admin, updateProduct)
 
 export default router
