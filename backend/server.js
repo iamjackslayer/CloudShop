bdcb4ab003193bbf7bd21ac36e5c3892239655e5
Jack Wong <jackwong3101@yahoo.com>
2020-12-16 16:34:28 +0800

    add uploadRoutes using multer

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRouter from './routes/productRoutes.js'
 import userRouter from './routes/userRoutes.js'
 import orderRouter from './routes/orderRoutes.js'
+import uploadRouter from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -17,6 +19,10 @@ app.use(express.json())
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
 app.use('/api/orders', orderRouter)
+app.use('/api/upload', uploadRouter)
+
+const __dirname = path.resolve() // cus __dirname is a commonJs syntax, but we're using ES module
+app.use('/uploads', express.static(path.join(__dirname, '/uploads'))) // make /uploads accessible to the browser by making /uploads a static folder
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
